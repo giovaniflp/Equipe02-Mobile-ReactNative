55ecc9d00596e0ad17545a968ddcc8f5c9f5b902
giovaniflp <giovanifeitosa123@gmail.com>
2024-02-09 00:46:36 -0300

    Settando o ambiente e configurações iniciais

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex justify-center items-center h-screen w-screen">
+      <Text className="text-center text-green-500">Equipe 02 - Mobile usando NativeWind - React Native + Tailwindcss!!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
